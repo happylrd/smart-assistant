cc1dd07517d762145b16be1352f63f9522f56b37
Diego Li <282108074@qq.com>
2017-02-18 13:36:28 +0800

    Create project structure

---
+++ SmartAssistant/app/src/main/java/io/happylrd/smartassistant/application/BaseApplication.java
@@ -0,0 +1,11 @@
+package io.happylrd.smartassistant.application;
+
+import android.app.Application;
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ SmartAssistant/app/src/main/java/io/happylrd/smartassistant/utils/StaticClass.java
@@ -0,0 +1,5 @@
+package io.happylrd.smartassistant.utils;
+
+public class StaticClass {
+
+}
